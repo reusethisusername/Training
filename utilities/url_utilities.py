f147e5eebcecea2276fe4ebe27f5779a3d75d0d2
Marius D <marius.cristi@gmail.com>
2018-02-15 22:29:45 +0200

    Finished url_utilities.py

--- utilities/url_utilities.py
+++ utilities/url_utilities.py
@@ -41,8 +41,8 @@ def scrape_page(page_contents: str):
         clean = True
 
         # no punctuation
-        for punc in string.punctuation:
-            if punc in word:
+        for punctuation_marks in string.punctuation:
+            if punctuation_marks in word:
                 clean = False
 
                 # no numbers
