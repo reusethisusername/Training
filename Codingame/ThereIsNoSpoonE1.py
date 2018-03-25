00e10e68b9bf9ac6e12782becd40a737af5d62ea
Marius D <marius.cristi@gmail.com>
2018-03-25 12:59:23 -0700

    Add partial sol for ThereIsNo

--- Codingame/ThereIsNoSpoonE1.py
+++ Codingame/ThereIsNoSpoonE1.py
@@ -1,15 +1,25 @@
 '''
 Created on May 16, 2017
-
 '''
 
-width = int(input())  # the number of cells on the X axis
-height = int(input())  # the number of cells on the Y axis
-L = []
+# width = int(input())  # the number of cells on the X axis
+# height = int(input())  # the number of cells on the Y axis
 
-for i in range(height):
-    # width characters, each either 0 is node or . is empty
-    L.append(list(input()))
+# width = int(input())  # the number of cells on the X axis
+# height = int(input())  # the number of cells on the Y axis
+# L = []
+
+# L = ['00','0.']  # 2x2
+L = ['0.0.0']  # horizontal
+#L = ['0', '0', '0', '0']  # vertical
+
+width = len(L[0])
+height = len(L)
+
+# for i in range(height):
+# width characters, each either 0 is node or . is empty
+# L.append(list(input()))
+
 APU = [['' for r in range(height)] for c in range(width)]
 for c in range(width):
     for r in range(height):
@@ -17,17 +27,33 @@ for c in range(width):
 
 for r in range(height):
     for c in range(width):
+        s = "{} {} ".format(c, r)
         if APU[c][r] == "0":
-            break
-            # check if it has right N
-            # check if it has bottom N
-            # print string
 
-            # node, node to right, node to bottom
+            try:
+                if APU[c + 1][r] == "0":  # check if it has right N
+                    s = s + "{} {} ".format(c + 1, r)
+                else:
+                    s = s + "{} {} ".format(-1, -1)
+            except IndexError:
+                s = s + "{} {} ".format(-1, -1)
+            try:
+                if APU[c][r + 1] == "0":  # check if it has bottom N
+                    s = s + "{} {} ".format(c, r + 1)
+                else:
+                    s = s + "{} {} ".format(-1, -1)
+            except IndexError:
+                s = s + "{} {} ".format(-1, -1)
 
-print(APU)
-print(APU[0][1], '\n', end='')
-print(APU[1][1], '\n', end='')
+            print(s.rstrip()) if len(s) > 10 else exit()
+
+        elif APU[c][r] == ".":
+            pass
+        # node, node to right, node to bottom
+
+# print(APU)
+# print(APU[0][1], '\n', end='')
+# print(APU[1][1], '\n', end='')
 
 # Three coordinates: a node, its right neighbor, its bottom neighbor
-#print("0 0 1 0 0 1")
+# print("0 0 1 0 0 1")
